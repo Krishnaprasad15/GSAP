9b3d80cfa4a99c95d363ac24fa00e1c3773a72d3
krishna1500 <krishna16151@gmail.com>
2023-10-29 14:04:22 +0530

    Stagger and timelines

--- script.js
+++ script.js
@@ -7,16 +7,16 @@
 
 // IF WE WANT TO ROTATE THE BOX WE SIMPLY USE THE ROTATE:__ DEGREES
 
-gsap.to("#box2",{
-    x:1000,
-    y:300,
-    backgroundColor:"red",
-    duration:2,
-    delay:1,
-    rotate:360,
-    repeat:-1
-    // scale:0.5
-})
+// gsap.to("#box2",{
+//     x:1000,
+//     y:300,
+//     backgroundColor:"red",
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     // repeat:-1
+//     // scale:0.5
+// })
 
 // gsap.from("#box2",{
 //     x:1000,
@@ -25,3 +25,64 @@ gsap.to("#box2",{
 //     delay:1,
 //     rotate:360,
 // })
+
+// ***** ANIMATING TEXTS *****
+
+// gsap.to("h1",{
+//     x:600,
+//     duration:3,
+//     delay:1,
+//     color:"black"
+//     // repeat:-1
+// })
+// gsap.to("h2",{
+//     x:600,
+//     duration:3,
+//     delay:2,
+//     color:"black"
+//     // repeat:-1
+// })
+// gsap.to("h3",{
+//     x:600,
+//     duration:3,
+//     delay:3,
+//     // repeat:-1
+//     color:"black"
+// })
+
+// ***** ANIMATING THE SAME TAG TEXTS ****
+
+// gsap.to("h1",{
+//     x:600,
+//     duration:2,
+//     dealy:1,
+//     color:"orange",
+//     stagger:1, // stagger is used in the case of same tag elements so that all the texts with same tag won't move at the same time according to the stagger dealy : 1s 2s ....
+//     repeat:-1, // used to repeat the animation for an infinite animation we use -1 value
+//     yoyo:true // boolean value of T/F for repeating the animation
+// })
+
+
+// **** TIMELINE ****
+
+// IT IS A PROCESS WHERE THE ANIMATIONS TAKES IN SYNCHRONOUS ORDER I.E IF THERE ARE MULTIPLE STRINGS TO BE ANIMATED THEY ANIMATE ACCORDING TO THE DELAY IN ASYNCHRONOUS ORDER BUT TO ANIMATE IN ORDER IRRESPECTIVE OF THE DELAY WE USE TIMELINE
+let tl= gsap.timeline()
+
+tl.to("h1",{
+    x:600,
+    duration:1,
+    delay:3,
+    color:"black"
+})
+tl.to("h2",{
+    x:600,
+    duration:1,
+    delay:1,
+    color:"black"
+})
+tl.to("h3",{
+    x:600,
+    duration:1,
+    delay:2,
+    color:"black"
+})
